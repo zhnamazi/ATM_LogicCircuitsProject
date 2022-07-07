f2f86e4f00db92ba4c7862a36b5037917fa8b99c
Zahra <znamazi81@gmail.com>
2022-07-07 17:28:49 +0430

    ATM design

--- ATM_tb.v
+++ ATM_tb.v
@@ -1,19 +1,16 @@
 `timescale 1ns/1ns
-`include "ten_bit_adder.v"
-`include "ten_bit_subtractor.v"
+`include "ATM_design.v"
 
 module ATM_tb;
 
     reg [0:1] Select;
-    reg [0:3] Acc_numbers[0:15]; 
-    reg [0:9] Balances [0:15];
-    //reg [0:3] AccountNumber_s, AccountNumber_d;
-    //reg [0:9] Amount;
-    //wire [0:9] Balance;
-    //wire [0:1] result;
+    reg [0:3] AccountNumber_s, AccountNumber_d;
+    reg [0:9] Amount;
+    wire [0:9] Balance;
+    wire [0:1] result;
 
-    //ten_bit_subtractor ins(a, b, result);
-    integer  i;
+    ATM ins(Select, Amount, AccountNumber_s, AccountNumber_d, result, Balance);
+
     initial begin
         
         $dumpfile("ATM_tb.vcd");
@@ -21,11 +18,43 @@ module ATM_tb;
 
         $display("0:Khorooj  1:Mojodi  2:Bardasht  3:Enteghal");
 
-        for(i = 0 ; i < 16 ; i = i++) begin
-            Balances[i] = 100;
-        end
 
-        //$display("%d", result);
+        Select = 1; AccountNumber_s = 4; AccountNumber_d = 8; Amount = 50; #10;
+        Select = 2; AccountNumber_s = 3; AccountNumber_d = 8; Amount = 70; #10;
+        Select = 3; AccountNumber_s = 6; AccountNumber_d = 10; Amount = 40; #10;
+        Select = 0; AccountNumber_s = 4; AccountNumber_d = 8; Amount = 50; #10;
+        Select = 3; AccountNumber_s = 4; AccountNumber_d = 8; Amount = 50; #10;
+        Select = 1; AccountNumber_s = 4; AccountNumber_d = 8; Amount = 50; #10;
+
+
+        if(Select == 0) begin
+
+            $display("0: Closed");
+            $finish;
+
+        end else if(Select==1) begin
+
+            $display("Balance: %d", Balance);
+
+        end else if(Select ==2) begin
+
+            if(result==3) begin
+                $display("Receive: %d", Amount);
+            end else begin
+                $display("Not enough balance");
+            end
+
+        end else begin
+
+            if(result==3) begin
+                $display("Transfer: %d", Amount);
+            end else begin
+                $display("Not enough balance");
+            end
+
+        end
+        
+
     end
     
 
